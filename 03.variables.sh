98ae10eddff4cdc742bad4f2afbe75a5a612168f
Manisha Yedla <manisha.yedla@gmail.com>
2024-04-16 16:42:20 -0500

    make changes

---
+++ 03.variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+
+echo "Minni:: "Siri, why dont you sleep?"
+echo "Deepika:: "Minni, I'm studying right now?"
+echo  "Minni:: "Its too late and past your bed time"
+echo "Siri:: "Sure, I will go to bed now."
+
+
+
