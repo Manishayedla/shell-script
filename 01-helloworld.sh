ae31ddecb0613ecfcbef66a780ca22b492295b81
Manisha Yedla <manisha.yedla@gmail.com>
2024-04-16 15:11:07 -0500

    add text

--- 01-helloworld.sh
+++ 01-helloworld.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
 echo "Hello World"
-echo "Add new text"
-echo "Add new text1"
-echo "Add new text2"
-echo "Add new text2"
-echo "Add new text2"
-echo "Add new text2"
-echo "Add new text2"
-echo "Add new text2"
-echo "Add new text2"
-echo "Add new sample"
-echo "Add new sample2"
---
+++ README.md
@@ -0,0 +1,11 @@
+# Commands
+
+```sh
+# Do it from your local terminal
+git add .
+git commit -m "message"
+git push origin main
+
+# Do it on EC2 instance
+git pull
+```
