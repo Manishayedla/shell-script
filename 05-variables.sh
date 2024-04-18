adb1afc5f086e41f027b078b971dcd77b465fbfc
Manisha Yedla <manisha.yedla@gmail.com>
2024-04-18 18:09:12 -0500

    enter manually

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter your ID::"
+
+read ID #here ID is the variable
+
+echo "Please enter your password::"
+
+read PASSWORD
+
+echo "ID is: $ID, Password is: $PASSWORD"
