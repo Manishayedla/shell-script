d7628c884954ac10105a1c13a8e19c9cf02fc6b3
Manisha Yedla <manisha.yedla@gmail.com>
2024-04-18 18:40:55 -0500

    MAKE CHANGES

---
+++ 06-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MEDICINE=("DOLO" "CITROGEN" "AZIN")
+
+# size of above array is 3.
+# index are 0,1,2
+
+# list always starts with 0.
+
+echo "First Medicine is: ${MEDICINE[0]}"
+echo "First Medicine is: ${MEDICINE[1]}"
+echo "First Medicine is: ${MEDICINE[@]}"
