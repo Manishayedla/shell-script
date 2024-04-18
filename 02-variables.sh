6bfa1cf30eec410ecbe6b91412cd3fc1cd892678
Manisha Yedla <manisha.yedla@gmail.com>
2024-04-18 12:19:52 -0500

    add text here

--- 02-variables.sh
+++ 02-variables.sh
@@ -5,3 +5,4 @@ echo Siri:: "Minni, I am studying right now"?
 echo Minni:: "What!! Its past you bed time.Go to sleep"
 echo Siri:: "Sure, I will!"
 echo Minni:: "Siri, Make sure that you'll sleep on time"
+echo Siri:: "Minni, sure"
