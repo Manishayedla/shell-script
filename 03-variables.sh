a2ba63fd5d617b412948115599839b27332d7105
Manisha Yedla <manisha.yedla@gmail.com>
2024-04-18 12:55:16 -0500

    make the changes

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+PERSON1=Minni #no space between = and value
+PERSON2=Siri
 
 
-echo Minni:: "Siri, What are you doing?"
-echo Siri:: "Minni, I am studying right now"?
-echo Minni:: "What!! Its past you bed time.Go to sleep"
-echo Siri:: "Sure, I will!
+echo $PERSON1:: "$PERSON2, What are you doing?"
+echo $PERSON2:: "$PERSON1, I am studying right now"?
+echo $PERSON1:: "What!! Its past you bed time.Go to sleep"
+echo $PERSON2:: "Sure, I will!"
+echo $PERSON1:: "$PERSON2, Make sure that you'll sleep on time"
+echo $PERSON2:: "$PERSON1, sure"
