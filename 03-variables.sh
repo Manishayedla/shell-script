cc6acc84a58f735c6352d5aef2ea92fe1e1fdb3b
Manisha Yedla <manisha.yedla@gmail.com>
2024-04-18 12:06:25 -0500

    make the changes

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+
+
 echo Minni:: "Siri, What are you doing?"
 echo Siri:: "Minni, I am studying right now"?
 echo Minni:: "What!! Its past you bed time.Go to sleep"
-echo Siri:: "Sure, I will!"
-echo Minni:: "Siri, Make sure that you'll sleep on time"
+echo Siri:: "Sure, I will!
