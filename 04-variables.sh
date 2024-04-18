238c9a803eb6c418dd194e4302ed019269fdf389
Manisha Yedla <manisha.yedla@gmail.com>
2024-04-18 17:40:41 -0500

    make the changes

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-
-#declaring of variable (we are creating here by saying p1=minni,p2=siri)
-PERSON1=Minni #no space between = and value
-PERSON2=Siri
+# $1 and $2 are arguments. If we want to change the names of person1 n 2 in future we can use arguemnts instead of change the whole program.
+PERSON1=$1 #no space between = and value
+PERSON2=$2
 
 #referring variable (we are using it here) (adding $ in front of it to help shell-script to recognise it has a variable and run the prgram )
 echo $PERSON1:: "$PERSON2, What are you doing?"
