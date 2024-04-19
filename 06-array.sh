7499e90eccb36d501709b4eca94f4b8ba644b473
Manisha Yedla <manisha.yedla@gmail.com>
2024-04-18 19:19:14 -0500

    MAKE CHNGES

---
+++ 06-array.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MEDICINE=("DOLO" "CITROGEN" "AZIN") #who can add the whole list here within double quotes and spaces)
+
+# size of above array is 3. #size always starts from 1,2,3...)
+# index are 0,1,2
+
+# list always starts with 0.
+
+echo "First Medicine is: ${MEDICINE[0]}"
+echo "Second Medicine is: ${MEDICINE[1]}"
+echo "Third Medicine is: ${MEDICINE[2]}"
