933c0b16f44d298ac45fdc361b0adda1599aca6a
Manisha Yedla <manisha.yedla@gmail.com>
2024-04-25 18:59:17 -0500

    RUN COLORS

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -8,6 +8,12 @@ LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 #temp directory lo scriptname - what time is has been executed 
 #creates a log file #where to send it? then we use redirections
 
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 VALIDATE() { 
 
 if [ $1 -ne 0 ] 
@@ -37,8 +43,8 @@ dnf list installed $i &>>$LOGFILE
 
 if [ $? -eq 0 ]  #if exit status is equal to zero then print already installed...skipping
 then
-echo "$i already installed.. SKIPPING" 
+echo -e "$i already installed.. $R SKIPPING $N" 
 else
-echo "$i not installed.. need to install"
+echo -e "$i not installed.. $Y need to install $N"
 fi
 done
