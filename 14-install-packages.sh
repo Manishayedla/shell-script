b5a3ff698cb9df365f7504b4161ca10c28286b21
Manisha Yedla <manisha.yedla@gmail.com>
2024-04-25 18:51:28 -0500

    run

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-USERID=$(id -u)
+
+USERID=$(id -u) #how do you run a command inside shellscript and take the output
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log  
+#temp directory lo scriptname - what time is has been executed 
+#creates a log file #where to send it? then we use redirections
+
+VALIDATE() { 
+
+if [ $1 -ne 0 ] 
+then 
+echo "$2.. FAILURE"
+exit 1
+else
+echo "$2.. SUCCESS"
+fi
+}
+
 if [ $USERID -ne 0 ] 
 then 
 
@@ -12,8 +30,15 @@ fi
 
 echo "All packages: $@"
 
-for i in $@
+for i in $@  #when we input values for i then we are runng if else.. by printing package install and if its equal print installed else not installed
 do 
 echo "package to install: $i"
-dnf list installed $i
+dnf list installed $i &>>$LOGFILE
+
+if [ $? -eq 0]  #if exit status is equal to zero then print already installed...skipping
+then
+echo "$i already installed.. SKIPPING" 
+else
+echo "$i not installed.. need to install"
+fi
 done
