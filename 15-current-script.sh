ef0b5f67e3cab5d53b05af6231b1765b9226a729
Manisha Yedla <manisha.yedla@gmail.com>
2024-05-20 19:29:32 -0500

    run

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -6,12 +6,12 @@ COURSE="DevOps from Current Script"
 echo "DevOps calling other script, course: $COURSE"
 echo "Process ID of current shell script: $$"
 
-./16-other-script.share
+./16-other-script.sh
 
 #after calling other script we are calling again.
 
 echo "DevOps calling other script, course: $COURSE"
-echo "1234"
+
 
 
 
