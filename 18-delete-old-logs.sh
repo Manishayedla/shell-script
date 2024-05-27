af52e0eec0ce4ed3d766caee1b23072998c03110
Manisha Yedla <manisha.yedla@gmail.com>
2024-05-27 17:18:11 -0400

    run

--- 18-delete-old-logs.sh
+++ 18-delete-old-logs.sh
@@ -12,7 +12,7 @@ N="\e[0m"
 #when working with directories and folders you to need to check if it exists or not in the first place.
 if [ -d $SOURCE_DIRECTORY ]
 then 
-echo -e " Source directory exists "
+echo -e "$G Source directory exists $N"
 else
 echo -e "$R Please make sure $SOURCE_DIRECTORY exists $N"
 exit 1
