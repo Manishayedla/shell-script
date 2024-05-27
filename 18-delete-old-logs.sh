74ebf3ed21dabece56325ae357d41982f09c1750
Manisha Yedla <manisha.yedla@gmail.com>
2024-05-27 16:53:30 -0400

    run

---
+++ 18-delete-old-logs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SOURCE_DIRECTORY=/tmp/app-logs
+
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+#when working with directories and folders you to need to check if it exists or not in the first place.
+if [ -d $SOURCE_DIRECTORY ]
+then 
+echo "$G Source directory exists $N"
+else
+echo "$R Please make sure $SOURCE_DIRECTORY exists $N"
+exit 1
+fi
